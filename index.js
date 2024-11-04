c7581ef425f110025c9c8c89bb0ad97dc8b753d6
Abdirahman005 <abdirahman.abdullah@student.moringaschool.com>
2024-11-04 11:50:37 +0300

    initial commit

---
+++ index.js
@@ -0,0 +1,5 @@
+document.getElementById('contact-form').addEventListener('submit', function(event) {
+    event.preventDefault();  // Prevents form from submitting the normal way
+    alert('Thank you for reaching out! I will get back to you soon.');
+});
+
